277d65d9572df3a27697b6bc0ad23dd1bd59cd92
Florece Ann Robles <annr.dev@gmail.com>
2024-03-23 15:04:43 -0400

    created the form group

--- src/app/form-validation/form-validation.component.ts
+++ src/app/form-validation/form-validation.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-validation',
@@ -9,5 +9,26 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './form-validation.component.less'
 })
 export class FormValidationComponent {
+  formValidation: FormGroup;
+  colors = ['White', 'Black', 'Orange', 'Blue', 'Red'];
 
+  constructor(private fb: FormBuilder) { 
+    this.formValidation = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      middleName: new FormControl(''),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, Validators.required),
+      phone: new FormControl(null, Validators.required),
+      age: new FormControl(null, Validators.required),
+      postcode: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+      size: new FormControl(null, Validators.required),
+      color: new FormControl('White'),
+      condition: new FormControl(null, Validators.required),
+    })    
+  }
+
+  onSubmit() {
+    console.warn(this.formValidation.value);
+  }  
 }
