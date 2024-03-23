b83b673f43b0ba7f29016f003827b40778ed1047
Florece Ann Robles <annr.dev@gmail.com>
2024-03-23 17:10:47 -0400

    added fields for code validation

--- src/app/code-validator/code-validator.component.ts
+++ src/app/code-validator/code-validator.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-code-validator',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './code-validator.component.html',
   styleUrl: './code-validator.component.less'
 })
 export class CodeValidatorComponent {
+  formValidation: FormGroup;
+  
+  constructor(private fb: FormBuilder) { 
+    this.formValidation = new FormGroup({
+      description: new FormControl(null, Validators.required),      
+    })    
+  }
 
+  onSubmit() {
+    console.warn(this.formValidation.value);
+  }
+
+  onClear() {
+    this.formValidation.reset();
+  }
 }
