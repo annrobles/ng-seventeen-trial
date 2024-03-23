e1f1bfb1b447f4ead2a6491eb830150099fc9e61
Florece Ann Robles <annr.dev@gmail.com>
2024-03-23 17:26:50 -0400

    added code validator

--- src/app/code-validator/code-validator.component.ts
+++ src/app/code-validator/code-validator.component.ts
@@ -10,7 +10,8 @@ import { FormControl, FormGroup, FormBuilder, ReactiveFormsModule, Validators } 
 })
 export class CodeValidatorComponent {
   formValidation: FormGroup;
-  
+  result = "Valid";
+
   constructor(private fb: FormBuilder) { 
     this.formValidation = new FormGroup({
       description: new FormControl(null, Validators.required),      
@@ -18,10 +19,59 @@ export class CodeValidatorComponent {
   }
 
   onSubmit() {
+    this.validateCode();
     console.warn(this.formValidation.value);
   }
 
   onClear() {
     this.formValidation.reset();
   }
+
+	validateCode() {
+    const chars: CharsMap = {
+      "<": ">",
+      "[": "]",
+      "{": "}",
+      "(": ")"
+    };
+    const counter: CounterMap = {
+      "<": 0,
+      "[": 0,
+      "{": 0,
+      "(": 0,
+      ">": 0,
+      "]": 0,
+      "}": 0,
+      ")": 0
+    };
+    const code = this.formValidation.get('description')?.value ?? "";
+    let stack: string[] = [];
+
+    for (let char of code) {
+      const key = char as keyof CharsMap;
+      if (chars[key]) {
+        stack.push(char);
+      } else if (Object.values(chars).includes(char)) {
+        if (stack.length === 0 || chars[stack.pop() as keyof CharsMap] !== char) {
+          this.result = "Invalid";
+          return;
+        }
+      }
+      if (counter[char] !== undefined) {
+        counter[char]++;
+      }
+    }
+		this.result = `Valid;\n<: ${counter["<"]}, >: ${counter[">"]}, [: ${counter["["]}, ]: ${counter["]"]}, {: ${counter["{"]}, }: ${counter["}"]}, (: ${counter["("]}, ): ${counter[")"]}`;
+	};  
 }
+
+interface CharsMap {
+  "<": string;
+  "[": string;
+  "{": string;
+  "(": string;
+}
+interface CounterMap {
+  [key: string]: number;
+}
+
