4be0b0c4ff2b9d4b585631407a74831da01adc80
Florece Ann Robles <annr.dev@gmail.com>
2024-03-23 10:42:59 -0400

    Added route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { CodeValidatorComponent } from './code-validator/code-validator.component';
+import { CryptoCurrencyComponent } from './crypto-currency/crypto-currency.component';
+import { FormValidationComponent } from './form-validation/form-validation.component';
+import { TodoComponent} from './todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: CryptoCurrencyComponent,
+        title: 'Crypto Currency'
+    },
+    {
+        path: 'todo',
+        component: TodoComponent,
+        title: 'To Do List'
+    },
+    {
+        path: 'code-validator',
+        component: CodeValidatorComponent,
+        title: 'Code Validator'
+    },
+    {
+        path: 'form-validation',
+        component: FormValidationComponent,
+        title: 'Form Validation'
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];
