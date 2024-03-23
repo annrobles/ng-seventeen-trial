4da253c947e0f8cb74e69dee00ad5b8372562710
Florece Ann Robles <annr.dev@gmail.com>
2024-03-23 10:11:14 -0400

    Added the initial set-up of components

---
+++ src/app/code-validator/code-validator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-validator',
+  standalone: true,
+  imports: [],
+  templateUrl: './code-validator.component.html',
+  styleUrl: './code-validator.component.less'
+})
+export class CodeValidatorComponent {
+
+}
---
+++ src/app/crypto-currency/crypto-currency.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-currency',
+  standalone: true,
+  imports: [],
+  templateUrl: './crypto-currency.component.html',
+  styleUrl: './crypto-currency.component.less'
+})
+export class CryptoCurrencyComponent {
+
+}
