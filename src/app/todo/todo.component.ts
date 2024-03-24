65d91da3e8b3f52302ac27289161852f522dead6
Florece Ann Robles <annr.dev@gmail.com>
2024-03-23 20:47:53 -0400

    added the todo input fields

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.less'
 })
 export class TodoComponent {
+  task = '';
+  tasks: string[] = [];
 
+  onSubmit(value: string) {
+    this.tasks.push(value);
+    this.task = '';
+  }  
 }
